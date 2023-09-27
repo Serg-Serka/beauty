049e715c4e5383a966c33034e568d0e535695877
serg <sergij.stadnyk@gmail.com>
2023-09-27 17:01:31 +0300

    Added models and migrations

---
+++ database/migrations/2023_09_26_212050_create_services_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('working_days');
+            $table->string('working_hours');// interval is 30mins
+            $table->boolean('is_enabled');
+            $table->unsignedBigInteger('salon_id');
+            $table->unsignedBigInteger('type_id');
+
+            $table->foreign('salon_id')->references('id')->on('salons');
+            $table->foreign('type_id')->references('id')->on('service_types');
+
+            $table->comment('Services');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
